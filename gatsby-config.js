81b7d08e3abf1da89310dcee824596b089eacb5a
ivan-business <109082795+ivanBusiness@users.noreply.github.com>
2022-10-06 21:25:01 +0400

    upgrade project

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata: {
+    title: `gatsby-email-template`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  // plugins: [{
+  //   resolve: 'gatsby-plugin-google-analytics',
+  //   options: {
+  //     "trackingId": "0f000"
+  //   }
+  // }]
+};
